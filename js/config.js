55c08985b96cfbf910a9d79b429d361821e36a97
andygi <andy.greco@gekode.co.uk>
2019-09-12 09:59:03 +0100

    add test file

---
+++ js/config.js
@@ -0,0 +1,7 @@
+requirejs.config({
+    baseurl: '/js',
+    paths: {
+        jquery: 'https://code.jquery.com/jquery-3.4.1.min',
+        lazyload: 'https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/8.10.0/lazyload.min'
+    }
+});
